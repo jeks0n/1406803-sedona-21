9d2a336ed684c9119474227edf1224ac2bbc4521
Evgenii Popov <jekson87@me.com>
2021-01-25 13:01:17 +0300

    Доработка по состоянию nojs

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,10 @@ const navToggleButton = document.querySelector('.main-nav');
 const mainNavOpened = 'main-nav--opened';
 const mainNavClosed = 'main-nav--closed';
 
+navToggleButton.classList.remove('main-nav--nojs');
+navToggleButton.classList.remove(mainNavOpened);
+navToggleButton.classList.add(mainNavClosed);
+
 navToggleButton.addEventListener('click', function (evt) {
   if (!navToggleButton.classList.contains(mainNavClosed)) {
     navToggleButton.classList.remove(mainNavOpened);
